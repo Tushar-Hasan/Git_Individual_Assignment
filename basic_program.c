d023980cdf2cce4f7349f7b3aa57f50bfd68abcc
Tushar Hasan <mahmudol.tushar@brainstation-23.com>
2024-04-25 10:35:39 +0600

    added a function to calculate the factorial of a number from feature-branch

--- basic_program.c
+++ basic_program.c
@@ -2,7 +2,18 @@
 
 int main()
 {
-
+clea
 printf("Hello World");
 return 0;
+}
+
+int factorial( int n) {
+    if( n <=0 ){
+        return -1;
+    }
+    int result = 1, i;
+    for (i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
